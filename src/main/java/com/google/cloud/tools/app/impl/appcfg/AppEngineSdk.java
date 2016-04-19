8ef5f46d98036d9f7cdadb2fc8cacb12bf1a5e9b
Mike Eltsufin <meltsufin@google.com>
2016-04-19 17:54:02 -0400

    fixed commented out tests

--- src/main/java/com/google/cloud/tools/app/impl/appcfg/AppEngineSdk.java
+++ src/main/java/com/google/cloud/tools/app/impl/appcfg/AppEngineSdk.java
@@ -1,5 +1,7 @@
 package com.google.cloud.tools.app.impl.appcfg;
 
+import com.google.appengine.tools.admin.AppCfg;
+
 import java.nio.file.Path;
 import java.util.List;
 
@@ -20,7 +22,7 @@ public class AppEngineSdk {
   public void runCommand(List<String> args) {
     // AppEngineSdk requires this system property to be set.
     System.setProperty("appengine.impl.root", appengineSdk.toString());
-    com.google.appengine.tools.admin.AppCfg.main(args.toArray(new String[args.size()]));
+    AppCfg.main(args.toArray(new String[args.size()]));
   }
 
 }
