e6bc503f088c630d89ed0966e2153cdc43d71047
Elliotte Rusty Harold <elharo@google.com>
2016-12-01 19:49:44 -0500

    unused import

--- src/main/java/com/google/cloud/tools/appengine/cloudsdk/serialization/CloudSdkVersion.java
+++ src/main/java/com/google/cloud/tools/appengine/cloudsdk/serialization/CloudSdkVersion.java
@@ -17,7 +17,6 @@
 package com.google.cloud.tools.appengine.cloudsdk.serialization;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -26,9 +25,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Represents a Version of the Cloud SDK, which follows the Semantic Version 2.0.0 spec. See
- * <a href="http://semver.org/spec/v2.0.0.html">http://semver.org/spec/v2.0.0.html</a> for more
- * detail.
+ * Represents a Version of the Cloud SDK, which follows the
+ * <a href="http://semver.org/spec/v2.0.0.html">Semantic Version 2.0.0 spec</a> for more detail.
  */
 public class CloudSdkVersion implements Comparable<CloudSdkVersion> {
 
