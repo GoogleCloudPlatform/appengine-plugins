3d74c71cf42a5f577129decc7a670b7312acc7cb
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-23 12:58:45 -0400

    invoke codecov.io

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -9,3 +9,4 @@ cd github/appengine-plugins-core
 sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 mvn clean install cobertura:cobertura -B -U
+bash <(curl -s https://codecov.io/bash)
