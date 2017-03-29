536fdecbe446226dd791f99bb2cf8bf76f72090e
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-29 14:03:59 -0400

    fix path for gcloud install

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -2,9 +2,9 @@ cd github/appengine-plugins-core
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
 GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
-google\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
-google\gcloud.cmd components update --quiet
-google\gcloud.cmd components install app-engine-java --quiet
+t:\google\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
+t:\google\gcloud.cmd components update --quiet
+t:\google\gcloud.cmd components install app-engine-java --quiet
 
 mvn clean install cobertura:cobertura -B -U
 curl -s https://codecov.io/bash | bash
