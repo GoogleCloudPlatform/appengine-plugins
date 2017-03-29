63ffb8fb888f80532ced7e97dd3404d1ea7837a4
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-29 14:56:19 -0400

    wait for installer exe to finish

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,7 +1,7 @@
 cd github/appengine-plugins-core
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
-GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
+start /WAIT GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
 call t:\google\google-cloud-sdk\bin\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
 call t:\google\google-cloud-sdk\bin\gcloud.cmd components update --quiet
 call t:\google\google-cloud-sdk\bin\gcloud.cmd components install app-engine-java --quiet
