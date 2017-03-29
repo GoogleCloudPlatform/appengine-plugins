c323d83eff9d2b1cc01467f72cbb37e0d105acb1
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-29 14:51:52 -0400

    use call to execute gcloud.cmd

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -2,9 +2,9 @@ cd github/appengine-plugins-core
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
 GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
-t:\google\google-cloud-sdk\bin\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
-t:\google\google-cloud-sdk\bin\gcloud.cmd components update --quiet
-t:\google\google-cloud-sdk\bin\gcloud.cmd components install app-engine-java --quiet
+call t:\google\google-cloud-sdk\bin\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
+call t:\google\google-cloud-sdk\bin\gcloud.cmd components update --quiet
+call t:\google\google-cloud-sdk\bin\gcloud.cmd components install app-engine-java --quiet
 
 mvn clean install cobertura:cobertura -B -U
 curl -s https://codecov.io/bash | bash
