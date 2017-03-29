af33b39d4940e1e2b8c7a09a09e252cc889f25f4
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-29 13:57:31 -0400

    install gcloud during windows build

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,6 +1,11 @@
 cd github/appengine-plugins-core
-gcloud components update
-gcloud components install app-engine-java
+
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
+GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=C:\google
+gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
+gcloud.cmd components update --quiet
+gcloud.cmd components install app-engine-java --quiet
+
 mvn clean install cobertura:cobertura -B -U
 curl -s https://codecov.io/bash | bash
 
