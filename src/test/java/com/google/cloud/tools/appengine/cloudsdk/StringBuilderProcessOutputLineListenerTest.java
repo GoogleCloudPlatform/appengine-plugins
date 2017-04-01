1085eb296b42abca2b7544939202e0849a7b6f91
Elliotte Rusty Harold <elharo@google.com>
2017-04-01 07:59:00 -0400

    Move StringBuilderProcessOutputLineListener out of internal so Eclipse can use it

--- src/test/java/com/google/cloud/tools/appengine/cloudsdk/StringBuilderProcessOutputLineListenerTest.java
+++ src/test/java/com/google/cloud/tools/appengine/cloudsdk/StringBuilderProcessOutputLineListenerTest.java
@@ -14,19 +14,18 @@
  * limitations under the License.
  */
 
-package com.google.cloud.tools.appengine.cloudsdk.internal.process;
+package com.google.cloud.tools.appengine.cloudsdk;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.cloud.tools.appengine.cloudsdk.process.StringBuilderProcessOutputLineListener;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for {@link StringBuilderProcessOutputLineListener}
  */
-@RunWith(MockitoJUnitRunner.class)
 public class StringBuilderProcessOutputLineListenerTest {
 
   private StringBuilderProcessOutputLineListener listener;
