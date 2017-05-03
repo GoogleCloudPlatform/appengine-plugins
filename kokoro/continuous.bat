da73f54fd1a20728ff2f6575a0685b5a18fd6ebd
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-03 10:48:09 -0400

    cleanup win build script

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,9 +1,9 @@
+@echo on
+
 cd github/appengine-plugins-core
 
-rem call gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
 call gcloud.cmd components update --quiet
 call gcloud.cmd components install app-engine-java --quiet
-rem set GOOGLE_CLOUD_SDK_HOME=t:\google\google-cloud-sdk
 
 mvn --version
 mvn clean install cobertura:cobertura -B -U
