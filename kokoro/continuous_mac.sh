252324192fe3e208137317c8354e6d55c90b623f
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-12 14:08:37 -0400

    use maven wrapper

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -5,8 +5,9 @@ set -e
 # Display commands to stderr.
 set -x
 
-cd github/appengine-plugins-core
 gcloud components update
 gcloud components install app-engine-java
-mvn clean install cobertura:cobertura -B -U
+
+cd github/appengine-plugins-core
+./mvnw clean install cobertura:cobertura -B -U
 # bash <(curl -s https://codecov.io/bash)
