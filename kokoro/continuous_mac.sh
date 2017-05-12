9b254c02ea57bbcd2eb03e6c5eba7fa6bb88255a
Andras Kerekes <akerekes@users.noreply.github.com>
2017-05-12 13:59:52 -0400

    Update continuous_mac.sh

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -5,8 +5,18 @@ set -e
 # Display commands to stderr.
 set -x
 
+curl https://sdk.cloud.google.com | bash
+GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
+"$GOOGLE_CLOUD_SDK_HOME"/bin/gcloud components install app-engine-java
+
+# temporary workaround until mvn is available in the image by default
+# the integration tests rely on Maven being installed, cannot use the wrapper
+curl http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
+tar -xzf apache-maven-3.5.0-bin.tar.gz
+
+M2_HOME="$(pwd)"/apache-maven-3.5.0
+PATH=$PATH:$M2_HOME/bin
+
 cd github/appengine-plugins-core
-gcloud components update
-gcloud components install app-engine-java
-mvn clean install cobertura:cobertura -B -U
+./mvnw clean install cobertura:cobertura -B -U
 # bash <(curl -s https://codecov.io/bash)
