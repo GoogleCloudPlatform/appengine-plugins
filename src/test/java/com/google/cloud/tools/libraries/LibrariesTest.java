bb7d80bf199f64d881c33bbea0561a37bbb7c8d2
Elliotte Harold <elharo@google.com>
2017-08-21 17:31:30 -0400

    json

--- src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
+++ src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
@@ -1,7 +1,10 @@
 package com.google.cloud.tools.libraries;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -9,6 +12,10 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
+
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonReaderFactory;
 
 public class LibrariesTest {
 
@@ -19,6 +26,15 @@ public class LibrariesTest {
     DocumentBuilder parser = factory.newDocumentBuilder();
     File in = new File("src/main/java/com/google/cloud/tools/libraries/libraries.xml");
     parser.parse(in);
+  }
+  
+  @Test
+  public void testJson() throws FileNotFoundException {
+    JsonReaderFactory factory = Json.createReaderFactory(null);
+    InputStream in =
+        new FileInputStream("src/main/java/com/google/cloud/tools/libraries/libraries.json");
+    JsonReader reader = factory.createReader(in); 
+    reader.read();
   }
 
 }
