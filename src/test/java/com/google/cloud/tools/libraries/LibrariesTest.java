8a42ecd8d9b87b0e264dd64cad4c67f107a001d7
Elliotte Harold <elharo@google.com>
2017-08-22 15:07:19 -0400

    expand test

--- src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
+++ src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -34,7 +36,7 @@ public class LibrariesTest {
   }
   
   @Test
-  public void testJson() throws FileNotFoundException {
+  public void testJson() throws FileNotFoundException, URISyntaxException {
     JsonReaderFactory factory = Json.createReaderFactory(null);
     InputStream in =
         new FileInputStream("src/main/java/com/google/cloud/tools/libraries/libraries.json");
@@ -42,14 +44,19 @@ public class LibrariesTest {
     Iterator<JsonValue> apis = reader.readArray().iterator();
     Assert.assertTrue(apis.hasNext());
     for (JsonObject api = (JsonObject) apis.next(); apis.hasNext(); api = (JsonObject) apis.next()) {
-       JsonArray clients = api.getJsonArray("clients");
-       Assert.assertFalse(clients.isEmpty());
-       for (int i = 0; i < clients.size(); i++) {
-         JsonObject client = (JsonObject) clients.get(i);
-         String status = client.getString("status");
-         Assert.assertTrue(status, 
-             "beta".equals(status) || "alpha".equals(status) || "GA".equals(status));
-       }
+    
+      new URI(api.getString("documentation"));
+      if (api.getString("icon") != null) {
+        new URI(api.getString("icon"));
+      }
+      JsonArray clients = api.getJsonArray("clients");
+      Assert.assertFalse(clients.isEmpty());
+      for (int i = 0; i < clients.size(); i++) {
+        JsonObject client = (JsonObject) clients.get(i);
+        String status = client.getString("status");
+        Assert.assertTrue(status, 
+            "beta".equals(status) || "alpha".equals(status) || "GA".equals(status));
+      }
     }
   }
 
