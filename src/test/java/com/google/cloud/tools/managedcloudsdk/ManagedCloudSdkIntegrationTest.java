b16a2f49cb0e1254e0f9abefa7e5479e20fa1f32
Appu Goundan <appu@google.com>
2017-11-17 18:00:42 -0500

    Provides an entry point into the Managed SDK mechanism.
    Also adds in an expensive test : ManagedCloudSdkIntegrationTest

---
+++ src/test/java/com/google/cloud/tools/managedcloudsdk/ManagedCloudSdkIntegrationTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.managedcloudsdk;
+
+import com.google.cloud.tools.managedcloudsdk.command.CommandExecutionException;
+import com.google.cloud.tools.managedcloudsdk.command.CommandExitException;
+import com.google.cloud.tools.managedcloudsdk.command.CommandRunner;
+import com.google.cloud.tools.managedcloudsdk.components.SdkComponent;
+import com.google.cloud.tools.managedcloudsdk.install.SdkInstallerException;
+import com.google.cloud.tools.managedcloudsdk.install.UnknownArchiveTypeException;
+import java.io.IOException;
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+/** Integration tests for {@link ManagedCloudSdk} and all supporting classes. */
+public class ManagedCloudSdkIntegrationTest {
+
+  @Rule public TemporaryFolder tempDir = new TemporaryFolder();
+
+  private static final String FIXED_VERSION = "169.0.0";
+  private final MessageCollector testListener = new MessageCollector();
+  private final SdkComponent testComponent = SdkComponent.APP_ENGINE_JAVA;
+
+  @Rule
+  public TestWatcher watchman =
+      new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+          System.out.println(description + "\n" + testListener.getOutput());
+        }
+      };
+
+  @Test
+  public void testManagedCloudSdk_versionedInstall()
+      throws BadCloudSdkVersionException, UnsupportedOsException, IOException, CommandExitException,
+          InterruptedException, ManagedSdkVerificationException, ManagedSdkVersionMismatchException,
+          UnknownArchiveTypeException, CommandExecutionException, SdkInstallerException {
+    ManagedCloudSdk testSdk =
+        new ManagedCloudSdk(
+            new Version(FIXED_VERSION), tempDir.getRoot().toPath(), OsInfo.getSystemOsInfo());
+
+    Assert.assertFalse(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.hasComponent(testComponent));
+    Assert.assertFalse(testSdk.isUpToDate());
+
+    testSdk.newInstaller().install(testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.hasComponent(testComponent));
+    Assert.assertTrue(testSdk.isUpToDate());
+
+    testSdk.newComponentInstaller().installComponent(testComponent, testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertTrue(testSdk.hasComponent(testComponent));
+    Assert.assertTrue(testSdk.isUpToDate());
+
+    // make sure we cant update a versioned cloud sdk
+    Assert.assertEquals(null, testSdk.newUpdater());
+  }
+
+  @Test
+  public void testManagedCloudSdk_latestInstall()
+      throws UnsupportedOsException, ManagedSdkVerificationException,
+          ManagedSdkVersionMismatchException, InterruptedException, CommandExecutionException,
+          CommandExitException, UnknownArchiveTypeException, IOException, SdkInstallerException {
+    ManagedCloudSdk testSdk =
+        new ManagedCloudSdk(Version.LATEST, tempDir.getRoot().toPath(), OsInfo.getSystemOsInfo());
+
+    Assert.assertFalse(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.hasComponent(testComponent));
+    Assert.assertFalse(testSdk.isUpToDate());
+
+    testSdk.newInstaller().install(testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.hasComponent(testComponent));
+    Assert.assertTrue(testSdk.isUpToDate());
+
+    testSdk.newComponentInstaller().installComponent(testComponent, testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertTrue(testSdk.hasComponent(testComponent));
+    Assert.assertTrue(testSdk.isUpToDate());
+  }
+
+  @Test
+  public void testManagedCloudSdk_updateLatest()
+      throws ManagedSdkVerificationException, UnsupportedOsException, InterruptedException,
+          ManagedSdkVersionMismatchException, CommandExecutionException, CommandExitException,
+          UnknownArchiveTypeException, IOException, SdkInstallerException {
+    ManagedCloudSdk testSdk =
+        new ManagedCloudSdk(Version.LATEST, tempDir.getRoot().toPath(), OsInfo.getSystemOsInfo());
+
+    Assert.assertFalse(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.isUpToDate());
+
+    testSdk.newInstaller().install(testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertTrue(testSdk.isUpToDate());
+
+    // forcibly downgrade the cloud SDK so we can test updating.
+    CommandRunner.newRunner()
+        .run(
+            Arrays.asList(
+                testSdk.getGcloud().toString(),
+                "components",
+                "update",
+                "--quiet",
+                "--version=" + FIXED_VERSION),
+            null,
+            null,
+            testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertFalse(testSdk.isUpToDate());
+
+    testSdk.newUpdater().update(testListener);
+
+    Assert.assertTrue(testSdk.isInstalled());
+    Assert.assertTrue(testSdk.isUpToDate());
+  }
+
+  private static class MessageCollector implements MessageListener {
+    StringBuilder output = new StringBuilder("");
+
+    @Override
+    public void message(String rawString) {
+      output.append(rawString);
+    }
+
+    public String getOutput() {
+      return output.toString();
+    }
+  }
+}
