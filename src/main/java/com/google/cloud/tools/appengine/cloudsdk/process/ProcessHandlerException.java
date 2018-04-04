75feb0432f11d4016eb46a6ed235f621ac54d052
Appu Goundan <appu@google.com>
2018-04-04 18:22:15 -0400

    Refactor out the process management from Cloud SDK task
    - Also removed unused cloud sdk operations
    - Removes inheritIO option from process handling
    - Adds in processHander to give developer access to raw process
    - Adds in LegacyProcessHandler to bridge the gap for users using
      the old mechanisms.

--- src/main/java/com/google/cloud/tools/appengine/cloudsdk/process/ProcessHandlerException.java
+++ src/main/java/com/google/cloud/tools/appengine/cloudsdk/process/ProcessHandlerException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Google Inc.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,16 @@
  * limitations under the License.
  */
 
-package com.google.cloud.tools.appengine.api.logs;
+package com.google.cloud.tools.appengine.cloudsdk.process;
 
-import com.google.cloud.tools.appengine.api.AppEngineException;
+/** Exception during execution of a {@link ProcessHandler}. */
+public class ProcessHandlerException extends Exception {
 
-/** This set of commands can be used to manage existing App Engine logs. */
-public interface AppEngineLogs {
+  public ProcessHandlerException(Exception cause) {
+    super(cause);
+  }
 
-  /** Reads log entries for the given App Engine application. */
-  void read(LogsConfiguration configuration) throws AppEngineException;
+  public ProcessHandlerException(String message) {
+    super(message);
+  }
 }
