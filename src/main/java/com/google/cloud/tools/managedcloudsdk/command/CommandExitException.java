f22765d9d138832c57e1d157f61bb0b9b4fcd669
Elliotte Harold <elharo@google.com>
2018-05-08 19:03:52 -0400

    checkstyle

--- src/main/java/com/google/cloud/tools/managedcloudsdk/command/CommandExitException.java
+++ src/main/java/com/google/cloud/tools/managedcloudsdk/command/CommandExitException.java
@@ -27,7 +27,7 @@ public class CommandExitException extends Exception {
    * Create a new exception.
    *
    * @param exitCode the process exit code
-   * @param errLog additional loggable error information, can be {@code null}
+   * @param errorLog additional loggable error information, can be {@code null}
    */
   public CommandExitException(int exitCode, String errorLog) {
     super("Process failed with exit code: " + exitCode);
