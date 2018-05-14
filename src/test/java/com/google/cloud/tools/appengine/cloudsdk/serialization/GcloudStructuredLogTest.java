e56609720b3bbabd3d07de2f3dc4860a904b312f
Chanseok Oh <chanseok@google.com>
2018-05-14 18:57:54 -0400

    Bonus

--- src/test/java/com/google/cloud/tools/appengine/cloudsdk/serialization/GcloudStructuredLogTest.java
+++ src/test/java/com/google/cloud/tools/appengine/cloudsdk/serialization/GcloudStructuredLogTest.java
@@ -17,11 +17,12 @@
 package com.google.cloud.tools.appengine.cloudsdk.serialization;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import com.google.cloud.tools.appengine.cloudsdk.JsonParseException;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class GcloudStructuredLogTest {
@@ -96,7 +97,7 @@ public class GcloudStructuredLogTest {
       GcloudStructuredLog.parse("non-JSON");
       fail();
     } catch (JsonParseException e) {
-      assertNotNull(e.getMessage());
+      assertThat(e.getMessage(), CoreMatchers.containsString("JsonSyntaxException"));
     }
   }
 
