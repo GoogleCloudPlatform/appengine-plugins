78b41283e1999c92083163edfb495b4430282dbb
Elliotte Harold <elharo@google.com>
2019-01-25 15:16:47 -0500

    more tests

--- src/test/java/com/google/cloud/tools/appengine/configuration/RunConfigurationTest.java
+++ src/test/java/com/google/cloud/tools/appengine/configuration/RunConfigurationTest.java
@@ -17,8 +17,12 @@
 package com.google.cloud.tools.appengine.configuration;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -57,4 +61,71 @@ public class RunConfigurationTest {
     List<String> flags = configuration.getJvmFlags();
     Assert.assertEquals(0, flags.size());
   }
+  
+  @Test
+  public void testBuilder() {
+  
+    List<String> inputFlags = new ArrayList<>();
+    inputFlags.add("foo");
+    inputFlags.add("bar");
+    List<Path> services = new ArrayList<>();
+    List<String> additionalArguments = new ArrayList<>();
+    Map<String, String> environment = new HashMap<>();
+    RunConfiguration configuration = RunConfiguration
+        .builder(services)
+        .additionalArguments(additionalArguments)
+        .adminHost("adminHost")
+        .adminPort(87)
+        .allowSkippedFiles(true)
+        .apiPort(88)
+        .authDomain("authDomain")
+        .automaticRestart(true)
+        .clearDatastore(true)
+        .customEntrypoint("customEntrypoint")
+        .datastorePath(Paths.get("/datastorepath"))
+        .defaultGcsBucketName("defaultGcsBucketName")
+        .devAppserverLogLevel("devAppserverLogLevel")
+        .environment(environment)
+        .host("host")
+        .jvmFlags(inputFlags)
+        .logLevel("logLevel")
+        .maxModuleInstances(54)
+        .port(999)
+        .projectId("projectId")
+        .pythonStartupArgs("pythonStartupArgs")
+        .pythonStartupScript("pythonStartupScript")
+        .runtime("runtime")
+        .skipSdkUpdateCheck(true)
+        .storagePath(Paths.get("/storagePath"))
+        .threadsafeOverride("threadsafeOverride")
+        .useMtimeFileWatcher(true)
+        .build();
+        
+    Assert.assertEquals(87, configuration.getAdminPort().intValue());        
+    Assert.assertEquals(88, configuration.getApiPort().intValue());        
+    Assert.assertEquals(999, configuration.getPort().intValue());        
+    Assert.assertEquals(54, configuration.getMaxModuleInstances().intValue());        
+
+    Assert.assertEquals("adminHost", configuration.getAdminHost());        
+    Assert.assertEquals("authDomain", configuration.getAuthDomain());        
+    Assert.assertEquals("customEntrypoint", configuration.getCustomEntrypoint());        
+    Assert.assertEquals("host", configuration.getHost());        
+    Assert.assertEquals("logLevel", configuration.getLogLevel());        
+    Assert.assertEquals("devAppserverLogLevel", configuration.getDevAppserverLogLevel());        
+    Assert.assertEquals("defaultGcsBucketName", configuration.getDefaultGcsBucketName());        
+    Assert.assertEquals("runtime", configuration.getRuntime());        
+    Assert.assertEquals("pythonStartupArgs", configuration.getPythonStartupArgs());        
+    Assert.assertEquals("pythonStartupScript", configuration.getPythonStartupScript());        
+    Assert.assertEquals("projectId", configuration.getProjectId());        
+            
+    Assert.assertEquals("/storagePath", configuration.getStoragePath().toString());        
+    Assert.assertEquals("/datastorepath", configuration.getDatastorePath().toString());        
+            
+    Assert.assertTrue(configuration.getUseMtimeFileWatcher());     
+    Assert.assertTrue(configuration.getSkipSdkUpdateCheck());     
+    Assert.assertTrue(configuration.getClearDatastore());     
+    Assert.assertTrue(configuration.getAutomaticRestart());     
+    Assert.assertTrue(configuration.getAllowSkippedFiles());
+  }
+  
 }
