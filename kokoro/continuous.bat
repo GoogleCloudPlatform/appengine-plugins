8b52796f7f77568ce1211d3e6d02cc3cb49b44ea
Chanseok Oh <chanseok@google.com>
2020-12-15 11:27:37 -0500

    test kokoro

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -4,14 +4,14 @@ REM Java 9 does not work with our builds right now, force java 8
 set JAVA_HOME=c:\program files\java\jdk1.8.0_152
 set PATH=%JAVA_HOME%\bin;%PATH%
 
+call curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk-windows-x86_64-bundled-python.zip
+call unzip google-cloud-sdk-windows-x86_64-bundled-python.zip
+call cmd.exe /c T:\tmp\google-cloud-sdk\install.bat
+
 cd github/appengine-plugins-core
 
 call gcloud.cmd components update --quiet
 call gcloud.cmd components install app-engine-java --quiet
-
-REM Latest Cloud SDK picks up c:\Python27\python.exe
-REM https://github.com/GoogleCloudPlatform/appengine-plugins-core/issues/851
-set CLOUDSDK_PYTHON=c:\Python37\python.exe
 
 call mvnw.cmd clean install cobertura:cobertura -B -U
 REM curl -s https://codecov.io/bash | bash
