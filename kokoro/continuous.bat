d91e0ed375035761757f0b005b346cb23d45c31f
Chanseok Oh <chanseok@google.com>
2020-12-15 11:35:33 -0500

    Remove debug code

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -4,10 +4,6 @@ REM Java 9 does not work with our builds right now, force java 8
 set JAVA_HOME=c:\program files\java\jdk1.8.0_152
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-call curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk-windows-x86_64-bundled-python.zip
-call unzip google-cloud-sdk-windows-x86_64-bundled-python.zip
-call cmd.exe /c T:\tmp\google-cloud-sdk\install.bat
-
 cd github/appengine-plugins-core
 
 call gcloud.cmd components update --quiet
