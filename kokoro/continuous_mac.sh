cd4cb24ea9aed0bc5db47dc5f6b12ef607a5444d
Mike Eltsufin <meltsufin@google.com>
2023-09-28 12:40:34 -0400

    Update continuous_mac.sh

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -10,5 +10,9 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
 gcloud components update
 gcloud components install app-engine-java
 
+# Use GCP Maven Mirror
+mkdir -p ${HOME}/.m2
+cp settings.xml ${HOME}/.m2
+
 cd github/appengine-plugins-core
 ./mvnw clean install cobertura:cobertura -B -U
