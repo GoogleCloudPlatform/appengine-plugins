4133c8d97d3e94625a7a4d1fc3806ac04e214b5b
Joe Wang <joewa@google.com>
2023-12-11 16:25:24 -0500

    chore: add stash.sh

---
+++ .kokoro/stage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+
+echo "${KOKORO_KEYSTORE_DIR}"
+
+cd github/appengine-plugins
+
+# Use GCP Maven Mirror
+mkdir -p "${HOME}"/.m2
+cp settings.xml "${HOME}"/.m2
+
+mvn -Prelease -B -U install -Dtest=!FilePermissionsTest -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS
