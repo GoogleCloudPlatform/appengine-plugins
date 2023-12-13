d5e4b72c90e372549dc66d19fbb3324bb524b01b
Joe Wang <joewa@google.com>
2023-12-12 19:18:43 -0500

    skip tests

--- .kokoro/stage.sh
+++ .kokoro/stage.sh
@@ -36,17 +36,17 @@ cp settings.xml "${HOME}"/.m2
 
 gcloud components install app-engine-java --quiet
 
-# run unit tests
-./mvnw -B -U \
---fail-at-end \
--DskipITs clean \
--Dtest=!FilePermissionsTest \
--Dorg.slf4j.simpleLogger.showDateTime=true \
--Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
-install
+## run unit tests
+#./mvnw -B -U \
+#--fail-at-end \
+#-DskipITs clean \
+#-Dtest=!FilePermissionsTest \
+#-Dorg.slf4j.simpleLogger.showDateTime=true \
+#-Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
+#install
 
 # stage release
-./mvnw deploy \
+./mvnw clean deploy \
 -Dorg.slf4j.simpleLogger.showDateTime=true \
 -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
 --batch-mode \
